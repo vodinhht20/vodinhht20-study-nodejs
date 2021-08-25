a117adc3fdffe4e6c3b023a253f817442213351f
vodinhht20 <vodinh2000ht@gmail.com>
2021-08-25 09:14:01 +0700

    create code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP LOGGER
+// app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+})
+app.get('/intro', function(req, res) {
+    res.render('intro');
+})
+
+app.listen(port)
---
+++ src/public/assets/javascript/main.js
@@ -0,0 +1,19 @@
+const qSelect = document.querySelector.bind(document);
+const qSelectAll = document.querySelectorAll.bind(document);
+
+const tabServices = qSelectAll('.service-tab-item');
+const tabServiceMains = qSelectAll('.service-main-item');
+
+tabServices.forEach((tabService, index) => { // index là số thứ tự của class khi click
+
+    const tabServicesMain = tabServiceMains[index];
+
+    tabService.onclick = function() {
+
+        qSelect('.service-tab-item.active').classList.remove('active'); // xóa các active đang có
+        this.classList.add('active'); // thêm class active vào nút mà mình đã nhấn
+
+        qSelect('.service-main-item.active').classList.remove('active');
+        tabServicesMain.classList.add('active');
+    }
+})
