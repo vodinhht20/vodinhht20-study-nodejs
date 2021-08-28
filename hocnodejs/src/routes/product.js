ccd97796557543bd5b1afe95a8ac9c03c0d9501c
Võ Định <71485569+vodinhht20@users.noreply.github.com>
2021-08-28 09:01:23 +0700

    Delete hocnodejs directory

--- hocnodejs/src/routes/product.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ProductController = require('../app/controllers/ProductController');
-
-router.get('/:slug', ProductController.show);
-
-module.exports = router;
--- hocnodejs/src/util/moongose.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mutipleMongooseToOject: function (mongooses) {
-    return mongooses.map((mongooses) => mongooses.toObject());
-  },
-  MongooseToOject: function (mongooses) {
-    return mongooses ? mongooses.toObject() : mongooses;
-  },
-
-};
